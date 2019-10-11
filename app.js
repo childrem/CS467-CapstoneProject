26836d474331444d6ad6a143ac5010bc2d2d417c
CroomOSU <37274399+CroomOSU@users.noreply.github.com>
2019-10-10 20:43:04 -0700

    fixed issue with ports

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var bodyParser = require('body-parser');
 var mysql = require('./dbcon.js');
 
+const PORT = process.env.PORT || 5000
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 10065);
+app.set('port', PORT);
 //Implementation for static found here:
 //https://expressjs.com/en/starter/static-files.html
 app.use('/static', express.static('public'));
