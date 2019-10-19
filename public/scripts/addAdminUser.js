37208d31c6c62dd68196a4af73b080004a6af0a7
childrem <childrem@oregonstate.edu>
2019-10-18 17:55:23 -0700

    started add admins functionality

--- public/scripts/addAdminUser.js
+++ public/scripts/addAdminUser.js
@@ -3,6 +3,31 @@ module.exports = function(){
     var router = express.Router();
 
   
+    function postAdmin(req, res, mysql, complete) {
+        mysql.pool.query("SELECT id FROM roles WHERE role = 'admin';", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+
+            complete();
+
+            let sql = "INSERT INTO users (email, password, role_id, date_created) VALUES (?,?,?,?);";
+            let inserts = [req.body.Email, req.body.Password, results[0].id, new Date()];
+            sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+                if(error) {
+                    res.write(JSON.stringify(error));
+                    res.end();
+                }
+
+                else {
+                    complete();
+                }
+            });
+        });
+    }
+
+
   // Display the admin homepage
   
   router.get('/', function(req, res){
@@ -11,6 +36,23 @@ module.exports = function(){
         res.render('addAdminUser', context);
     });
     
+
+    // When user submits a new admin's information, add it to database and send them back to the admin table
+
+    router.post('/', function(req, res){
+        let mysql = req.app.get('mysql');
+        let callbackCount = 0;
+        postAdmin(req, res, mysql, complete);
+        function complete() {
+            callbackCount++;
+            if(callbackCount >= 2) {
+                res.redirect('/viewAdmins');
+            }
+        }
+        //let sql = "INSERT INTO users (email, password, role_id) VALUES (?,?,?);";
+        //let inserts = [req.body.Email, req.body.Password, 1];   // role_id is 1 because this is an admin user
+
+    });
     
     return router;
 }();
