3529818bc85f4677fc830eee2a5bc1e586238f97
kevinhwang1995 <43596331+kevinhwang1995@users.noreply.github.com>
2019-10-22 16:34:59 -0700

    Added script files for User Homepage
    Added script files for User homepage

---
+++ public/scripts/currentEmployeeMonth.js
@@ -0,0 +1,14 @@
+module.exports = function(){
+    var express = require('express');
+    var router = express.Router();
+
+  
+  router.get('/', function(req, res){
+        let context = {};
+        context.userPage = true;
+        res.render('currentEmployeeMonth', context);
+    });
+    
+    
+    return router;
+}();
