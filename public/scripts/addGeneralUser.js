f74eadc286e2fe7e35baee539feb2b682805567b
CroomOSU <37274399+CroomOSU@users.noreply.github.com>
2019-10-25 19:21:29 -0700

    fixed wrong role in addGeneralUser.js

--- public/scripts/addGeneralUser.js
+++ public/scripts/addGeneralUser.js
@@ -18,7 +18,7 @@ module.exports = function(){
     bcrypt.hash(req.body.Password, saltRounds, function (err,   hash) {
       var date = new Date().toISOString().slice(0, 19).replace('T', ' ');
 
-      mysql.pool.query("INSERT INTO users (user_name, password, email, date_created, role_id ) VALUES (?,?,?,?,?)", [req.body.user_name, hash, req.body.Email, date, 1], function(err, result){
+      mysql.pool.query("INSERT INTO users (user_name, password, email, date_created, role_id ) VALUES (?,?,?,?,?)", [req.body.user_name, hash, req.body.Email, date, 11], function(err, result){
       if(err){
         next(err);
         return;
