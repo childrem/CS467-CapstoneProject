a79dc24535023b6a955c5b4ea022d44f86e5e3a7
childrem <childrem@oregonstate.edu>
2019-10-28 13:15:34 -0700

    added password hashing to create admin user
    implemented password hashing upon admin account creation based on user supplied password.

--- public/scripts/addAdminUser.js
+++ public/scripts/addAdminUser.js
@@ -3,6 +3,8 @@ module.exports = function(){
     var router = express.Router();
 
     var isAdmin = require('../../adminCheck.js');
+    var bcrypt = require('bcrypt');
+    const saltRounds = 10;
 
   
     function postAdmin(req, res, mysql, complete) {
@@ -14,17 +16,20 @@ module.exports = function(){
 
             complete();
 
-            let sql = "INSERT INTO users (email, password, role_id, date_created) VALUES (?,?,?,?);";
-            let inserts = [req.body.Email, req.body.Password, results[0].id, new Date()];
-            sql = mysql.pool.query(sql, inserts, function(error, results, fields){
-                if(error) {
-                    res.write(JSON.stringify(error));
-                    res.end();
-                }
+            bcrypt.hash(req.body.Password, saltRounds, function (err, hash) {
+                let date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+                let sql = "INSERT INTO users (email, password, role_id, date_created) VALUES (?,?,?,?);";
+                let inserts = [req.body.Email, hash, results[0].id, date];
+                sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+                    if(error) {
+                        res.write(JSON.stringify(error));
+                        res.end();
+                    }
 
-                else {
-                    complete();
-                }
+                    else {
+                        complete();
+                    }
+                });
             });
         });
     }
@@ -51,9 +56,6 @@ module.exports = function(){
                 res.redirect('/viewAdmins');
             }
         }
-        //let sql = "INSERT INTO users (email, password, role_id) VALUES (?,?,?);";
-        //let inserts = [req.body.Email, req.body.Password, 1];   // role_id is 1 because this is an admin user
-
     });
     
     return router;
