aac275738b2b2408ecc912cfcf629879dcf0188e
childrem <childrem@oregonstate.edu>
2019-11-04 13:12:40 -0800

    edit admins and general users working
    reworked the forms and set up handlers to get edit admins and general users working.
    tested via table inspection both on the website and MySQL workbench

--- public/scripts/editAdmin.js
+++ public/scripts/editAdmin.js
@@ -21,18 +21,49 @@ module.exports = function(){
 
     }
 
+    function editAdmin (req, res, mysql, complete) {
+        var sql = "UPDATE users SET email = ? WHERE id = ?";
+        var inserts = [req.body.Email, req.body.id];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
+            if(error){
+                res.write(JSON.stringify(error));
+                res.status(400).end();
+            }
+
+            else {
+                complete();
+            }
+        });
+    }
+
 
     router.get('/:id', isAdmin, function(req, res) {
 
         var callbackCount = 0;
         let context = {};
         context.adminPage = true;
+        context.id = req.params.id;     // Stored in a hidden input field, used later in edit process
         var mysql = req.app.get('mysql');
         getEmail(req, res, mysql, context, complete);
         function complete(){    // Each "setup" function calls this function to signal that it is finished
             callbackCount++;
             if(callbackCount >= 1){
                 res.render('editAdmin', context);  // Only render when context is all setup
+            }
+        }
+    });
+
+
+    // Using POST as method because PUT is not supported by HTML forms
+
+    router.post('/', isAdmin, function(req, res) {
+        let mysql = req.app.get('mysql');
+        var callbackCount = 0;
+        editAdmin(req, res, mysql, complete);
+        function complete() {
+            callbackCount++;
+            if(callbackCount >= 1) {
+                res.redirect('/viewAdmins');
             }
         }
     });
--- public/scripts/editGeneralUserAdminSite.js
+++ public/scripts/editGeneralUserAdminSite.js
@@ -24,11 +24,28 @@ module.exports = function(){
     }
 
 
+    function editGeneralUser(req, res, mysql, complete) {
+        var sql = "UPDATE users SET email = ?, user_name = ?, signature_path = ? WHERE id = ?";
+        var inserts = [req.body.Email, req.body.user_name, req.body.Signature, req.body.id];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
+            if(error) {
+                res.write(JSON.stringify(error));
+                res.status(400).end();
+            }
+
+            else {
+                complete();
+            }
+        });
+    }
+
+
     router.get('/:id', isAdmin, function(req, res) {
 
         var callbackCount = 0;
         let context = {};
         context.adminPage = true;
+        context.id = req.params.id;     // Stored in a hidden input field, used later in edit process
         var mysql = req.app.get('mysql');
         getAccountInfo(req, res, mysql, context, complete);
         function complete(){    // Each "setup" function calls this function to signal that it is finished
@@ -39,6 +56,20 @@ module.exports = function(){
         }
     });
 
+
+    // Using POST as method because PUT is not supported by HTML forms
+
+    router.post('/', isAdmin, function(req, res) {
+        let mysql = req.app.get('mysql');
+        var callbackCount = 0;
+        editGeneralUser(req, res, mysql, complete);
+        function complete() {
+            callbackCount++;
+            if(callbackCount >= 1) {
+                res.redirect('/viewGeneralUsers');
+            }
+        }
+    });
 
 
     return router;
