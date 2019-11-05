e7e66b485d79b668411cf814f49981fd303b4a3c
childrem <childrem@oregonstate.edu>
2019-11-05 14:46:07 -0800

    business intelligence foundation setup
    server is sending data to client side js and table is displaying on the webpage

--- public/scripts/businessIntelligence.js
+++ public/scripts/businessIntelligence.js
@@ -2,14 +2,36 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
+    var Chart = require('chart.js');
+
     var isAdmin = require('../../adminCheck.js');
+
   
-  // Display the admin homepage
+  // Display the business intelligence page (came from regular browser call - NOT chart request)
   
   router.get('/', isAdmin, function(req, res){
         let context = {};
         context.adminPage = true;
+        context.jsscripts = ["chartBuilder.js"];
+
+        //let data = {"xAxis": ["test1", "test2", "test3", "test4", "test5", "test6"]};
+        //res.json(data);
+
+        //context.chartType = 'bar';
+        //context.xAxis = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+        //context.chartLabel = '# of Votes';
+        //context.yAxis = [12, 19, 3, 5, 2, 3];
+
         res.render('businessIntelligence', context);
+    });
+
+
+    // Comes from AJAX on client side
+
+    router.get('/chartSample', isAdmin, function(req, res) {
+        var dataToSend = {"xAxis": ["test1", "test2", "test3", "test4", "test5", "test6"]};
+        var formattedDataToSend = JSON.stringify(dataToSend);
+        res.send(formattedDataToSend);
     });
     
     
---
+++ public/scripts/chartBuilder.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+
+    console.log("You're in the client side javascript");
+
+    LoadChart();
+
+    //var req = new XMLHttpRequest();
+    //var url = '/businessIntelligence/chartSample';
+
+    //req.open('GET',url,true);
+    //req.addEventListener('load',onLoad);
+    //req.addEventListener('error',onError);
+
+    //req.send();
+
+    //function onError() {
+        //console.log('error receiving async AJAX call');
+    //}
+
+});
+
+
+function LoadChart() {
+
+    console.log("You're in load chart");
+
+    var req = new XMLHttpRequest();
+    var url = 'businessIntelligence/chartSample';
+
+    req.open('GET',url,true);
+    req.addEventListener('load',onLoad);
+    req.addEventListener('error',onError);
+
+    req.send(null);
+}
+
+
+function onLoad() {
+
+    console.log("You're in onLoad");
+    
+    var response = this.responseText;
+    console.log(response);
+    var parsedResponse = JSON.parse(response);
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: parsedResponse['xAxis'], //['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'], //parsedResponse['xAxis'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }  
+    });
+
+}
+
+function onError() {
+    console.log('error receiving async AJAX call');
+}
