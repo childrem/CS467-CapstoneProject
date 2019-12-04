241c554bd2a79e8e94c8b307313eb938345abfd2
kevinhwang1995 <43596331+kevinhwang1995@users.noreply.github.com>
2019-12-04 11:41:33 -0800

    Removed password change + updated package file

--- public/scripts/editUserInfo.js
+++ public/scripts/editUserInfo.js
@@ -2,8 +2,6 @@ module.exports = function(){
     var express = require('express');
 	var isGeneral = require('../../generalUserCheck.js');
     var router = express.Router();
-	var bcrypt = require('bcrypt');
-	const saltRounds = 10;
 	
     function getInfo(req, res, mysql, context, complete) {
         var sql = "SELECT email, user_name FROM users WHERE id = ?";
@@ -23,20 +21,17 @@ module.exports = function(){
 
 
     function editInfo(req, res, mysql, complete) {
-		bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-			var sql = "UPDATE users SET email = ?, user_name = ?, password = ? WHERE id = ?";
-			var inserts = [req.body.email, req.body.user_name, hash, req.session.user_id];
-			sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
-				if(error) {
-					res.write(JSON.stringify(error));
-					res.status(400).end();
-				}
-
-				else {
-					complete();
-				}
-			});
-		})
+		var sql = "UPDATE users SET email = ?, user_name = ? WHERE id = ?";
+		var inserts = [req.body.email, req.body.user_name, req.session.user_id];
+		sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
+			if(error) {
+				res.write(JSON.stringify(error));
+				res.status(400).end();
+			}
+			else {
+				complete();
+			}
+		});
 	}
 
     router.get('/', isGeneral, function(req, res) {
